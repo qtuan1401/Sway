a2a1cf8f6d43a4d4b96834e4215162646bc42db6
qtuan <qtuan14012001@gmail.com>
2022-08-04 22:18:21 +1000

    Frontend ui

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,29 @@
+import HomeIcon from "./common-ui/HomeIcon";
+import MoreinfoIcon from "./common-ui/MoreinfoIcon";
+import RoundButton from "./common-ui/RoundButton";
+import OurmodelIcon from "./common-ui/OurmodelIcon";
+import ShareIcon from "./common-ui/ShareIcon";
+
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="nav-container">
+      <RoundButton>
+        <HomeIcon />
+      </RoundButton>
+      
+      <RoundButton>
+        <OurmodelIcon />
+      </RoundButton>
+
+      <RoundButton>
+        <MoreinfoIcon />
+      </RoundButton>
+
+      <RoundButton>
+        <ShareIcon />
+      </RoundButton>
+    </div>
+  );
+}
---
+++ frontend/src/common-ui/Button.js
@@ -0,0 +1,11 @@
+import './Button.css';
+
+export default function Button({ children }) {
+  return (
+    <div className='button-container'>
+      <button className="button">
+        {children}
+      </button>
+    </div>
+  );
+}
