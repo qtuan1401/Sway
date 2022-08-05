1834dc2060bf5be3c6939628364333a320ebf663
Sam Hobson <101384710+shobson2710@users.noreply.github.com>
2022-08-05 12:46:58 +1000

    Changed directory structure of backend

--- backend/keras.py
+++
@@ -1,9 +0,0 @@
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Activation, Dense, BatchNormalization, Conv2D
-from keras.optimizers import Adam
-from keras.metrics import categorical_crossentropy
-from keras.preprocessing.image import ImageDataGenerator
-
-
-
