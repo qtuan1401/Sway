3dd2dde0a20b1efda3c9b2eac2810cebdc91afde
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-09-07 12:21:03 +1000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import BiasDisplay from './components/BiasDisplay'
+import Navbar from './components/Navbar'
 
 function App() {
-  const descText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vestibulum sed arcu non odio euismod lacinia at. Quis eleifend quam adipiscing."
-
   return (
     <>
-      <BiasDisplay headerText="Political Bias" desc={descText} leftLabel="Low" rightLabel="High" barPercent="30%"/>
+      
     </>
   );
 }
