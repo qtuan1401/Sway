1ecaabe9f9896332a709d50d6a73aa2efaab2324
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-09-09 05:03:51 +1000

    Added input field component
    Some of the MUI parameters should be fiddled around with at some point to achieve a more simplistic look. The font may also need to be changed at some point for text in the field (currently Roboto).

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import BiasOverall from './components/BiasOverall'
 import Navbar from './components/Navbar'
 import LogoDisplay from './components/LogoDisplay'
 import Logo from './logos/sway_logo_transparent.png'
+import UserInputField from './components/UserInputField'
 
+// All imports above have been left in this file for testing purposes
 
 function App() {
 	return (
 		<>
-			
+			<UserInputField title={"URL"} rows={1} width={600}/>
+			<UserInputField title={"Text"} rows={10} width={600}/>
 		</>
 	);
 }
