50021edbb960ba026fd2773e9ea3df865bdb7f14
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-09-27 15:24:36 +1000

    Added page change logic to Navbar component and completed initial testing of page change
    Coded in logic that will allow the user to click buttons on the Navbar to render different pages. The page components are contained in the pages folder. Edit components in this folder to change any given page. There is probably a better way to do this than the way I've implemented but this way will definitely work and is relatively easy to work with.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,22 +2,40 @@ import React, { useState } from 'react'
 
 import './styles/Navbar.css'
 import Button from './navbar-components/Button'
+import PageController from './PageController'
 import logoHome from './navbar-components/button-icons/home.png'
 import logoMoreInfo from './navbar-components/button-icons/moreinfo.png'
 import logoOurModel from './navbar-components/button-icons/ourmodel.png'
 import logoShare from './navbar-components/button-icons/share.png'
+
+// Active Page Index
+// 0 - Home (Default)
+// 1 - Results
+// 2 - Our Model
+// 3 - About Us
+// 4 - N/A
+
+// Active page state deterimed by activePage variable below. Changes to this state can be made
+// by pressing buttons on the Navbar. This will change what is rendered on the rest of this page.
+// Not the most elegant solution for page changes but it'll do.
 
 const Navbar = () => {
     const [B1active, setB1Active] = useState(true);
     const [B2active, setB2Active] = useState(false);
     const [B3active, setB3Active] = useState(false);
     const [B4active, setB4Active] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     const handleClickB1 = () => {
         setB1Active(true);
         setB2Active(false);
         setB3Active(false);
         setB4Active(false);
+        if(activePage == 0) {
+            setActivePage(1);
+        } else {
+            setActivePage(0);
+        }
     }
 
     const handleClickB2 = () => {
@@ -25,6 +43,7 @@ const Navbar = () => {
         setB2Active(true);
         setB3Active(false);
         setB4Active(false);
+        setActivePage(2);
     }
 
     const handleClickB3 = () => {
@@ -32,6 +51,7 @@ const Navbar = () => {
         setB2Active(false);
         setB3Active(true);
         setB4Active(false);
+        setActivePage(3);
     }
 
     const handleClickB4 = () => {
@@ -39,23 +59,29 @@ const Navbar = () => {
         setB2Active(false);
         setB3Active(false);
         setB4Active(true);
+        setActivePage(4);
     }
 
     return (
-        <div className="navbarContainer">
-            <div className="buttonOne">
-                <Button active={B1active} onClick={handleClickB1} logo={logoHome} logoXOffset={10} logoYOffset={10}/>
+        <>
+            <div className="navbarContainer">
+                <div className="buttonOne">
+                    <Button active={B1active} onClick={handleClickB1} logo={logoHome} logoXOffset={10} logoYOffset={10}/>
+                </div>
+                <div className="buttonTwo">
+                    <Button active={B2active} onClick={handleClickB2} logo={logoOurModel} logoXOffset={10} logoYOffset={10}/>
+                </div>
+                <div className="buttonThree">
+                    <Button active={B3active} onClick={handleClickB3} logo={logoMoreInfo} logoXOffset={12} logoYOffset={9}/>
+                </div>
+                <div className="buttonFour">
+                    <Button active={B4active} onClick={handleClickB4} logo={logoShare} logoXOffset={16} logoYOffset={10}/>
+                </div>
             </div>
-            <div className="buttonTwo">
-                <Button active={B2active} onClick={handleClickB2} logo={logoOurModel} logoXOffset={10} logoYOffset={10}/>
+            <div className="pageContainer">
+                <PageController activePage={activePage}/>
             </div>
-            <div className="buttonThree">
-                <Button active={B3active} onClick={handleClickB3} logo={logoMoreInfo} logoXOffset={12} logoYOffset={9}/>
-            </div>
-            <div className="buttonFour">
-                <Button active={B4active} onClick={handleClickB4} logo={logoShare} logoXOffset={16} logoYOffset={10}/>
-            </div>
-        </div>
+        </>
     )
 }
 
---
+++ frontend/src/components/pages/AboutUsPage.js
@@ -0,0 +1,5 @@
+const AboutUsPage = () => {
+    
+}
+
+export default AboutUsPage;
