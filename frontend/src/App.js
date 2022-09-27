e1d60cbfc0ce2fb5be6e0c75493db686743a3790
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-09-28 00:12:11 +1000

    Added Homepage component and tested functionality
    Running npm start will open homepage by default. I would like to implement a way for the textboxes to switch between each other as the active textbox i.e. when the URL textbox is active, the text area should become empty and inactive. I will add this feature to the backlog.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import Navbar from './components/Navbar'
+import LogoDisplay from './components/LogoDisplay'
+import Logo from './logos/sway_logo_transparent.png'
 
 function App() {
 	return (
 		<>
+			<LogoDisplay img_src={Logo}/>
 			<Navbar/>
 		</>
 	);
--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,5 +1,32 @@
+import './styles/HomePage.css'
+
+import UserInputField from '../UserInputField'
+import SubmitButton from '../SubmitButton'
+
 const HomePage = () => {
-    
+    const handleSubmitButtonClick = () => {
+        // Add code here to handle transition to the results page
+    }
+
+    return (
+        <div className="homePage">
+            <div className="homePageHeader">
+                Enter text or a URL into the boxes below.
+            </div>
+            <div className="urlTextBox">
+                <UserInputField title={"URL"} width={1166}/>
+            </div>
+            <div className="orLabel">
+                OR
+            </div>
+            <div className="textInputBox">
+                <UserInputField title={"Text"} rows={20} width={1166}/>
+            </div>
+            <div className="homepageSubmitButton">
+                <SubmitButton onClick={handleSubmitButtonClick}/>
+            </div>
+        </div>
+    )
 }
 
 export default HomePage;
