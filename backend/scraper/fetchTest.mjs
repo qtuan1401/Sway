a98b360f821837d15573d5debd02bd282c1ceaeb
Sam Hobson <101384710+shobson2710@users.noreply.github.com>
2022-10-19 23:24:59 +1100

    Data preprocessing for Quality Model + further dir structure alterting

--- backend/scraper/fetchTest.mjs
+++
@@ -1,8 +0,0 @@
-import fetch from 'node-fetch'
-
-async function fetch_demo(){
-	fetch('https://www.google.com.au')
-	.then((response) => response.json())
-	.then((data) => console.log(data));;
-}
-fetch_demo();
