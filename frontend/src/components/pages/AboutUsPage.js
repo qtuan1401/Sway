1abe833410f67b32ee181a1bb866b09cf1368ef6
Sam Hobson <101384710+shobson2710@users.noreply.github.com>
2022-10-20 23:41:13 +1100

    Added Ethan info to About us page

--- frontend/src/components/pages/AboutUsPage.js
+++ frontend/src/components/pages/AboutUsPage.js
@@ -3,6 +3,7 @@ import Blank from './headshots/blank-profile-picture-973460_1280.webp'
 import Sam from './headshots/sam_headshot.png'
 import Aidan from './headshots/aidan.png'
 import Chrisvin from './headshots/chrisvin.png'
+import Ethan from './headshots/ethan.png'
 
 const AboutUsPage = () => {
     return (
@@ -46,7 +47,7 @@ const AboutUsPage = () => {
                                     <p class="title">Project Leader</p>
                                     <p>Hello, I'm Chris. I am doing a Bachelor of Computer Science degree with a major in Cyber security and Digital Systems security. I enjoy gaming, hiking, coding and playing my guitar. I hope that one day, I can use my knowledge to contribute in some way to space exploration.</p>
                                     <p>josephchrisvin@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("example1@example.com")}}>Copy Email</button></p>
+                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("josephchrisvin@gmail.com")}}>Copy Email</button></p>
                                 </div>
                             </div>
                         </div>
@@ -56,7 +57,7 @@ const AboutUsPage = () => {
                                 <img src={Sam} alt="Dev" width="100%"/>
                                 <div class="container">
                                     <h2>Sam Hobson</h2>
-                                    <p class="title">Backend Designer</p>
+                                    <p class="title">Machine Learning Engineer</p>
                                     <p>Hi! My name is Sam and I am about to complete a Bachelor of Computer Science, majoring in Cyber Security. I love playing games, skiing, astronomy and F1 racing. I plan on using my passion for Cyber Security to help protect computer systems in the future!</p>
                                     <p>sam.hobson2535@gmail.com</p>
                                     <p><button class="button" onClick={() => {navigator.clipboard.writeText("sam.hobson2535@gmail.com")}}>Copy Email</button></p>
@@ -66,13 +67,13 @@ const AboutUsPage = () => {
 
                         <div class="column">
                             <div class="card">
-                                <img src={Blank} alt="Dev" width="100%"/>
+                                <img src={Ethan} alt="Dev" width="100%"/>
                                 <div class="container">
                                     <h2>Ethan Moore</h2>
                                     <p class="title">Backend Designer</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                    <p>example@example.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("example@example.com")}}>Copy Email</button></p>
+                                    <p>Hey, I'm Ethan! I'm in my final year of University doing a Bachelor of Computer Science and majoring in Cyber Security. I'm interested in playing video games, listening to music, wrestling and big history! I'm hoping to effectively contribute to the Cyber Security Industry using my knowledge.</p>
+                                    <p>ecm628@gmail.com</p>
+                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("ecm628@gmail.com")}}>Copy Email</button></p>
                                 </div>
                             </div>
                         </div>
