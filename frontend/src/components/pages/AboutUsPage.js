42936d93d3ac9453e9df949f75e67b9ae5003dec
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-10-21 21:53:17 +1100

    Fixed loading bar position, spacings and some other stuff

--- frontend/src/components/pages/AboutUsPage.js
+++ frontend/src/components/pages/AboutUsPage.js
@@ -38,93 +38,90 @@ const AboutUsPage = () => {
 
                         <div class="aboutUsBodyTitle"> Our Team </div>
                         <br/>
-
-                        <div class="row">
-                        <div class="column">
-                            <div class="card">
-                                <img src={Chrisvin} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Chrisvin Joseph</h2>
-                                    <p class="title">Project Leader</p>
-                                    <p>Hello, I'm Chris. I am doing a Bachelor of Computer Science degree with a major in Cyber security and Digital Systems security. I enjoy gaming, hiking, coding and playing my guitar. I hope that one day, I can use my knowledge to contribute in some way to space exploration.</p>
-                                    <p>josephchrisvin@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("josephchrisvin@gmail.com")}}>Copy Email</button></p>
+                        <div className="ourTeamContainer">
+                            <div class="row">
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Chrisvin} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Chrisvin Joseph</h2>
+                                            <p class="title">Project Leader</p>
+                                            <p>Hello, I'm Chris. I am doing a Bachelor of Computer Science degree with a major in Cyber security and Digital Systems security. I enjoy gaming, hiking, coding and playing my guitar. I hope that one day, I can use my knowledge to contribute in some way to space exploration.</p>
+                                            <p>josephchrisvin@gmail.com</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("josephchrisvin@gmail.com")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="card">
-                                <img src={Sam} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Sam Hobson</h2>
-                                    <p class="title">Machine Learning Engineer</p>
-                                    <p>Hi! My name is Sam and I am about to complete a Bachelor of Computer Science, majoring in Cyber Security. I love playing games, skiing, astronomy and F1 racing. I plan on using my passion for Cyber Security to help protect computer systems in the future!</p>
-                                    <p>sam.hobson2535@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("sam.hobson2535@gmail.com")}}>Copy Email</button></p>
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Sam} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Sam Hobson</h2>
+                                            <p class="title">Machine Learning Engineer</p>
+                                            <p>Hi! My name is Sam and I am about to complete a Bachelor of Computer Science, majoring in Cyber Security. I love playing games, skiing, astronomy and F1 racing. I plan on using my passion for Cyber Security to help protect computer systems in the future!</p>
+                                            <p className="samEmail">sam.hobson2535@gmail.com</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("sam.hobson2535@gmail.com")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="card">
-                                <img src={Ethan} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Ethan Moore</h2>
-                                    <p class="title">Backend Designer</p>
-                                    <p>Hey, I'm Ethan! I'm in my final year of University doing a Bachelor of Computer Science and majoring in Cyber Security. I'm interested in playing video games, listening to music, wrestling and big history! I'm hoping to effectively contribute to the Cyber Security Industry using my knowledge.</p>
-                                    <p>ecm628@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("ecm628@gmail.com")}}>Copy Email</button></p>
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Ethan} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Ethan Moore</h2>
+                                            <p class="title">Backend Designer</p>
+                                            <p>Hey, I'm Ethan! I'm in my final year of University doing a Bachelor of Computer Science and majoring in Cyber Security. I'm interested in playing video games, listening to music, wrestling and big history! I'm hoping to effectively contribute to the Cyber Security Industry using my knowledge.</p>
+                                            <p>ecm628@gmail.com</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("ecm628@gmail.com")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="card">
-                                <img src={Blank} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Quoc Duong Nquyen</h2>
-                                    <p class="title">Backend Designer</p>
-                                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                                    <p>duongnq2k1@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("duongnq2k1@gmail.com")}}>Copy Email</button></p>
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Blank} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Quoc Duong Nquyen</h2>
+                                            <p class="title">Backend Designer</p>
+                                            <p>Hey I’m Michael. I’m in my final semester of Bachelor of Computer Science with a major in Big Data. I love racing games and motorsports and enjoy a good book. I’m hoping to apply my data skills acquired from uni to the healthcare and medical industry.</p>
+                                            <p className="michealEmail">duongnq2k1@gmail.com</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("duongnq2k1@gmail.com")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="card">
-                                <img src={Aidan} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Aidan Galovic</h2>
-                                    <p class="title">Frontend Designer</p>
-                                    <p>Hi, my name is Aidan and I am currently completing my final year of my Bachelor of Computer Science (Cyber Security) degree. I really enjoying getting outdoors and playing sports, I also love coding little games in my spare time. </p>
-                                    <p>ajg378@uowmail.edu.au</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("ajg378@uowmail.edu.au")}}>Copy Email</button></p>
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Aidan} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Aidan Galovic</h2>
+                                            <p class="title">Frontend Designer</p>
+                                            <p>Hi, my name is Aidan and I am currently completing my final year of my Bachelor of Computer Science (Cyber Security) degree. I really enjoying getting outdoors and playing sports, I also love coding little games in my spare time. </p>
+                                            <p>ajg378@uowmail.edu.au</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("ajg378@uowmail.edu.au")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="card">
-                                <img src={Theo} alt="Dev" width="100%"/>
-                                <div class="container">
-                                    <h2>Quoc Tuan Nguyen</h2>
-                                    <p class="title">Frontend Designer</p>
-                                    <p>Hey, I'm Tuan! I'm in my final year of Bachelor of Computer Science and majoring in Software Engineering. I'm interested in coding, travelling and sports. I hope I can use my knowledge to create good software applications.</p>
-                                    <p>nguyenquoctuannk@gmail.com</p>
-                                    <p><button class="button" onClick={() => {navigator.clipboard.writeText("nguyenquoctuannk@gmail.com")}}>Copy Email</button></p>
+                                <div class="column">
+                                    <div class="card">
+                                        <img src={Theo} alt="Dev" width="100%"/>
+                                        <div class="container">
+                                            <h2>Quoc Tuan Nguyen</h2>
+                                            <p class="title">Frontend Designer</p>
+                                            <p>Hey, I'm Tuan! I'm in my final year of Bachelor of Computer Science and majoring in Software Engineering. I'm interested in coding, travelling and sports. I hope I can use my knowledge to create good software applications.</p>
+                                            <p className="theoEmail">nguyenquoctuannk@gmail.com</p>
+                                            <p><button class="button" onClick={() => {navigator.clipboard.writeText("nguyenquoctuannk@gmail.com")}}>Copy Email</button></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-            </div>
-            <br/>
-
-
-            
+            <br/>  
         </div>
 
     )
