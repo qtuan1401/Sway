21d585d157c157fca7ad26e2bbccd99fcc54ff12
Chrisvin Joseph <chrisvinjoseph@gmail.com>
2022-10-21 20:20:24 +1100

    Reverted some changes to CSS
    Changes that allow for animations were kept.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,6 @@
 import Navbar from './components/Navbar'
 import LogoDisplay from './components/LogoDisplay'
 import Logo from './logos/sway_logo_transparent.png'
-import DiscreteBar from './components/biasDisplay-components/discreteBar'
-
-import './App.css'
 
 function App() {
 	return (
@@ -12,11 +9,6 @@ function App() {
 			<Navbar />
 		</>
 	);
-	// return (
-	// 	<>
-	// 		<DiscreteBar value={-1} confidence={1}/>
-	// 	</>
-	// )
 }
 
 export default App;
