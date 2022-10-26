7236ab2fec2c3a9fa74c648eb68b791cb7fb280a
qtuan <qtuan14012001@gmail.com>
2022-10-27 10:21:41 +1100

    Update CircularProgress color

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CircularProgress from '@mui/material/CircularProgress';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './styles/Loading.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+    },
+  },
+});
 
 function Loading() {
   return ReactDOM.createPortal(
     <div className="loading-overlay">
       <div className="loading-container">
-        <CircularProgress />
+        <ThemeProvider theme={theme}>
+          <CircularProgress size={75} />
+        </ThemeProvider>
       </div>
     </div>,
     document.querySelector('body'),
