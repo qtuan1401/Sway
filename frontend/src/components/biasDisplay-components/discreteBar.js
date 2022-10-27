a847c2fe72bd8e731dee53cee8e964a14e2566d9
qtuan1401 <qtuan14012001@gmail.com>
2022-10-27 12:16:18 +1100

    Update bar logic

--- frontend/src/components/biasDisplay-components/discreteBar.js
+++ frontend/src/components/biasDisplay-components/discreteBar.js
@@ -8,33 +8,33 @@ import './styles/discreteBar.css'
 // confidence - magnitude of movement in either direction
 
 const DiscreteBar = ({value, confidence}) => {
-    const [barPos, setBarPos] = useState(124)
-    const offsetToCenter = 124
+    const [barPos, setBarPos] = useState(20)
+    const offsetToCenter = 0;
 
     useEffect(() => {
-        if(value == 1 || value == -1) {
-            setBarPos(offsetToCenter + (124*confidence))
+        if(value === 1 || value === -1) {
+            setBarPos(20 + 80 * confidence)
         }
     }, [value, confidence])
-
-    if(value == 0) {
+    console.log(value, confidence, barPos);
+    if(value === 0) {
         return (
             <div className="discreteBarBG">
-                <div className="discreteBar" style={{width: "97px", left:`${barPos}px`}}>
+                <div className="discreteBar" style={{width: "30%", marginLeft: `${barPos}%`}}>
                 </div>
             </div>
         )
-    } else if(value == 1) {
+    } else if(value === 1) {
         return (
             <div className="discreteBarBG">
-                <div className="discreteBar" style={{width: "97px", left:`${barPos}px`}}>
+                <div className="discreteBar" style={{width: "30%", marginLeft: `${barPos}%`}}>
                 </div>
             </div>
         )
-    } else if(value == -1) {
+    } else if(value === -1) {
         return (
             <div className="discreteBarBG">
-                <div className="discreteBar" style={{width: "97px", right:`${barPos}px`}}>
+                <div className="discreteBar" style={{width: "30%", marginRight: `${barPos}%`}}>
                 </div>
             </div>
         )
